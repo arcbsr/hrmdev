ac1dfe0881145df211c376c75e8d9df0d86d948e
arcbsr <bsrsoftbd@gmail.com>
2022-08-07 20:02:37 +0600

    testing

--- api/views/kpi.py
+++ api/views/kpi.py
@@ -118,36 +118,37 @@ class GetDataZK(APIView):
     permission_classes = []
     authentication_classes = []
 
-    def get(self, request):
-        conn = None
-        zk = ZK('159.65.217.206', port=4370, timeout=5, password=0, force_udp=False, ommit_ping=False)
-        try:
-            # connect to device
-            conn = zk.connect()
-            # disable device, this method ensures no activity on the device while the process is run
-            conn.disable_device()
-            # another commands will be here!
-            # Example: Get All Users
-            users = conn.get_users()
-            for user in users:
-                privilege = 'User'
-                if user.privilege == const.USER_ADMIN:
-                    privilege = 'Admin'
-                print ('+ UID #{}'.format(user.uid))
-                print ('  Name       : {}'.format(user.name))
-                print ('  Privilege  : {}'.format(privilege))
-                print ('  Password   : {}'.format(user.password))
-                print ('  Group ID   : {}'.format(user.group_id))
-                print ('  User  ID   : {}'.format(user.user_id))
-            return Response({"connection": "Connected to ZKTecho and found data..."})
-            # Test Voice: Say Thank You
-            conn.test_voice()
-            # re-enable device after all commands already executed
-            conn.enable_device()
-        except Exception as e:
-            print ("Process terminate : {}".format(e))
-        finally:
-            if conn:
-                conn.disconnect()
+    def post(self, request):
+        print(request.data)
+        # conn = None
+        # zk = ZK('159.65.217.206', port=4370, timeout=5, password=0, force_udp=False, ommit_ping=False)
+        # try:
+        #     # connect to device
+        #     conn = zk.connect()
+        #     # disable device, this method ensures no activity on the device while the process is run
+        #     conn.disable_device()
+        #     # another commands will be here!
+        #     # Example: Get All Users
+        #     users = conn.get_users()
+        #     for user in users:
+        #         privilege = 'User'
+        #         if user.privilege == const.USER_ADMIN:
+        #             privilege = 'Admin'
+        #         print ('+ UID #{}'.format(user.uid))
+        #         print ('  Name       : {}'.format(user.name))
+        #         print ('  Privilege  : {}'.format(privilege))
+        #         print ('  Password   : {}'.format(user.password))
+        #         print ('  Group ID   : {}'.format(user.group_id))
+        #         print ('  User  ID   : {}'.format(user.user_id))
+        #     return Response({"connection": "Connected to ZKTecho and found data..."})
+        #     # Test Voice: Say Thank You
+        #     conn.test_voice()
+        #     # re-enable device after all commands already executed
+        #     conn.enable_device()
+        # except Exception as e:
+        #     print ("Process terminate : {}".format(e))
+        # finally:
+        #     if conn:
+        #         conn.disconnect()
         return Response({"message": "KPI submit to manger"})
 
