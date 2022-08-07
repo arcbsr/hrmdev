3a6537ecb87189d3e6e8b6dda0f35f6eb90c1424
arcbsr <bsrsoftbd@gmail.com>
2022-08-08 00:41:15 +0600

    - db setup

--- hrm/settings.py
+++ hrm/settings.py
@@ -88,29 +88,26 @@ WSGI_APPLICATION = 'hrm.wsgi.application'
 #     }
 # }
 
-POSTGRESQL = config('POSTGRESQL', cast=bool, default=False)
+# POSTGRESQL = config('POSTGRESQL', cast=bool, default=False)
 
-if POSTGRESQL:
-    DATABASES = {
+# if POSTGRESQL:
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DB_NAME', cast=str),
-            'USER': config('DB_USER', cast=str),
-            'PASSWORD': config('DB_PASSWORD', cast=str),
-            'HOST': config('DB_HOST', cast=str),
-            'PORT': config('DB_PORT', cast=str),
-            'TEST': {
-                'NAME': config('TEST_DB_NAME', 'kashboitest'),
-            },
+            'NAME': 'hrmdb',
+            'USER': 'hrmuser',
+            'PASSWORD': 'arc43211',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
